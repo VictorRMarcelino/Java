8fdc8b76bf4adb4199fc8a478709a57e367f220a
VictorRMarcelino <victorramosmarcelino@hotmail.com>
2022-08-13 18:12:02 -0300

    some changes in the system

--- src/marketSystem/Product.java
+++ src/marketSystem/Product.java
@@ -2,6 +2,16 @@ package marketSystem;
 
 public class Product {
     String name;
-    double price, kilo, quantity;
+    double price, kilo, quantity, finalPrice;
 
+
+    public String showFinalInformations(){
+        return name;
+    }
+    public void calcFinalPriceWithQuantity(){
+        finalPrice = price * quantity;
+    }
+    public void calcFinalPriceWithKilo(){
+        finalPrice = price * kilo;
+    }
 }
--- src/marketSystem/systemMarket.java
+++ src/marketSystem/systemMarket.java
@@ -7,16 +7,16 @@ import java.util.Scanner;
 public class systemMarket {
     static Scanner sc = new Scanner(System.in);
     static Product product = new Product();
-    static int finalPrice;
+    static double finalPrice;
     static List<String> productsNameList = new ArrayList<String>();
-    static List<String> productsPriceList = new ArrayList<String>();
-    static List<String> productsQuantityList = new ArrayList<String>();
-    static List<String> productsKiloList = new ArrayList<String>();
+    static List<Double> productsPriceList = new ArrayList<Double>();
+    static List<Double> productsQuantityList = new ArrayList<Double>();
+    static List<Double> productsKiloList = new ArrayList<Double>();
     public static void main(String[] args) {
         askEnterNewItem();
     }
     public static void askEnterNewItem(){
-        System.out.println("Do you want to add a new item in your list?");
+        System.out.println("Do you want to add a new item in your list? (y/n)");
         String answer;
         answer = sc.nextLine();
         switch (answer){
@@ -44,15 +44,21 @@ public class systemMarket {
         selectBetweenQuantityOrKilo();
     }
     public static void selectBetweenQuantityOrKilo(){
-        System.out.println("The product you want to add is priced by quantity or kilo?");
+        System.out.println("The product you want to add is priced by quantity or kilo? (quantity/kilo)");
         String answer;
         answer = sc.nextLine();
         switch (answer){
             case "quantity" ->{
                 enterTheQuantityOfTheProduct();
+                break;
             }
-            case "n" ->{
+            case "kilo" ->{
                 enterTheKiloOfTheProduct();
+                break;
+            }
+            default ->{
+                selectBetweenQuantityOrKilo();
+                break;
             }
         }
     }
@@ -61,17 +67,43 @@ public class systemMarket {
         double productQuantity;
         productQuantity = sc.nextDouble();
         product.quantity = productQuantity;
+        product.calcFinalPriceWithQuantity();
+        confirmInsert();
     }
     public static void enterTheKiloOfTheProduct(){
         System.out.println("Insert the quantity of the product");
         double productKilo;
         productKilo = sc.nextDouble();
         product.kilo = productKilo;
+        product.calcFinalPriceWithKilo();
+        confirmInsert();
     }
-    public static void calcFinalPriceWithQuantity(){
-        finalPrice = product.price * product.quantity;
+    public static void confirmInsert(){
+        product.showFinalInformations();
+        System.out.println("Do you want to add these informations to the list?");
+        String answer;
+        answer = sc.nextLine();
+        switch (answer){
+            case "y" ->{
+                insertTheInformationsInTheList();
+            }
+            case "n" ->{
+                askEnterNewItem();
+            }
+            default ->{
+                confirmInsert();
+            }
+        }
+    }
+    public static void insertTheInformationsInTheList(){
+        productsNameList.add(product.name);
+        productsPriceList.add(product.price);
+        productsQuantityList.add(product.quantity);
+        productsKiloList.add(product.kilo);
     }
     public static void showFinalList(){
-
+        for (int i = 0; i < productsNameList.size(); i++){
+            System.out.print(productsNameList.get(i));
+        }
     }
 }
